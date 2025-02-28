280f6812905d2fc84817a959747070589dd1ce41
lizeyliz <alizaeaton@gmail.com>
2025-02-28 09:19:25 -0800

    working on book class

--- Book.java
+++ Book.java
@@ -1,1 +1,12 @@
 //object class: template for book object
+class Book {
+    //instance variables
+    private String title;
+    private String author;
+    //take number off of internet (easier) OR create method to determine
+    private double dewey; //dewey decimal number of book;
+
+    //dewey number
+    // 1)first 3 digits are subject area (shelved in numerical order)
+    // 2) after 3 digits decimal, show the sub-section of the subject area (shelved in numerical order)
+}//end class Book
