a34febdf1f594e3de25b3ec9d7f8f7b8fb33ef36
lizeyliz <alizaeaton@gmail.com>
2025-03-02 15:22:05 -0800

    book class completed

--- Book.java
+++ Book.java
@@ -9,4 +9,32 @@ class Book {
     //dewey number
     // 1)first 3 digits are subject area (shelved in numerical order)
     // 2) after 3 digits decimal, show the sub-section of the subject area (shelved in numerical order)
+
+    //constructor
+    public Book(String title, String author, double dewey) {
+        this.title = title;
+        this.author = author;
+        this.dewey = dewey;
+    }
+
+    //getters and setters
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public double getDewey() {
+        return dewey;
+    }
+    public void setDewey(double dewey) {
+        this.dewey = dewey;
+    }
+    //end getters and setters
 }//end class Book
