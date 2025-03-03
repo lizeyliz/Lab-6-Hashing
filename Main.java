2916ab741636e8434b03d8e6a87819bb7d003b24
lizeyliz <alizaeaton@gmail.com>
2025-03-02 16:43:14 -0800

    saving work in main

--- Main.java
+++ Main.java
@@ -1,19 +1,39 @@
 //test class
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 public class Main {
     // Create a HashMap object called library
     // The key is the first digit of book's dewey #
     // and the value is the book toString method
     public static void main(String[] args) {
-        HashMap<Integer, String> library = new HashMap<>();
+        //hashmaps where each spot is list
+        HashMap<Integer, List<Book>> library = new HashMap<>();
+        
+        //add empty arraylists to each spot in library
+        for (int i = 0; i < 10; i++) {
+            library.put(i, new ArrayList<Book>());
+        }
+        
+        //traverse hashmap and place based on key
+        for (Map.Entry<Integer,List<Book>> mapElement : library.entrySet()) {
+            int key = mapElement.getKey();
+            System.out.println(key);
+        }
+
+
+
+
         Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 813.52);
         Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", 813.54);
         Book book3 = new Book("1984", "George Orwell", 813.54);
 
-        library.put(book1.getKey(), book1.toString());
-        library.put(book2.getKey(), book2.toString());
-        library.put(book3.getKey(), book3.toString());
+        //library.put(book1.getKey(), book1.toString());
+        //library.put(book2.getKey(), book2.toString());
+        //library.put(book3.getKey(), book3.toString());
 
         System.out.println(library);
     }//end main method
 }//end class Main
+
