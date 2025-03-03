8f91a9fa21cb2eac39b70809bd87577dc86744a6
lizeyliz <alizaeaton@gmail.com>
2025-03-02 16:54:53 -0800

    saving work in main (still not working)

--- Main.java
+++ Main.java
@@ -8,6 +8,8 @@ public class Main {
     // The key is the first digit of book's dewey #
     // and the value is the book toString method
     public static void main(String[] args) {
+
+        //INITIALIZE HASH MAP (empty)
         //hashmaps where each spot is list
         HashMap<Integer, List<Book>> library = new HashMap<>();
         
@@ -15,13 +17,16 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             library.put(i, new ArrayList<Book>());
         }
-        
+        //END INITIALIZE HASHMAP
+
+
+        //ADD BOOK
         //traverse hashmap and place based on key
         for (Map.Entry<Integer,List<Book>> mapElement : library.entrySet()) {
             int key = mapElement.getKey();
             System.out.println(key);
         }
-
+        //END ADD BOOK
 
 
 
@@ -33,7 +38,21 @@ public class Main {
         //library.put(book2.getKey(), book2.toString());
         //library.put(book3.getKey(), book3.toString());
 
+        addBook(book1, library);
+
         System.out.println(library);
     }//end main method
+
+    //ADD BOOK: adds a book to a linked list in the hashmap
+    public void addBook(Book book, HashMap<Integer, List<Book>> library){
+        //traverse hashmap and place based on key
+        for (Map.Entry<Integer,List<Book>> mapElement : library.entrySet()) {
+            //int key = mapElement.getKey();
+            //System.out.println(key);
+            if (mapElement.getKey() == 2|| mapElement.getKey() == 7){
+                System.out.println("Howdy");
+            }
+        }//end for loop
+    }//END ADD BOOK
 }//end class Main
 
